ef0f00ac868ba6a3abd36a18333f933e2c4cf7c1
Muhammad Asad <m.asad.chatthaa@gmail.com>
2024-07-10 11:39:21 +0500

    feat: add Semaphore screen and set as root VC

---
+++ TestUIKit/Controllers/Semaphore/SemaphoreVC.swift
@@ -0,0 +1,18 @@
+//
+//  SemaphoreVC.swift
+//  TestUIKit
+//
+//  Created by o9tech on 10/07/2024.
+//
+
+import UIKit
+
+class SemaphoreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
