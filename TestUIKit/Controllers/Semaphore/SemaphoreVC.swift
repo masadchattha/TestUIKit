e2e3d0a571cb3cc712249c7fdd5c6a249df10eac
Muhammad Asad <m.asad.chatthaa@gmail.com>
2024-07-10 12:04:04 +0500

    feat: downalod images from urls to mimic the issue which semaphore will resolve
    Problem Found: All downloads start at the same time, which can lead to high memory usage, and if this were real network requests, it might overload the network or server.
    
    Starting download from https://via.placeholder.com/150/92c952
    Starting download from https://via.placeholder.com/150/24f355
    Starting download from https://via.placeholder.com/150/d32776
    Starting download from https://via.placeholder.com/150/f66b97
    Starting download from https://via.placeholder.com/150/771796
    Finished download from https://via.placeholder.com/150/d32776
    Finished download from https://via.placeholder.com/150/771796
    Finished download from https://via.placeholder.com/150/f66b97
    Finished download from https://via.placeholder.com/150/92c952
    Finished download from https://via.placeholder.com/150/24f355

--- TestUIKit/Controllers/Semaphore/SemaphoreVC.swift
+++ TestUIKit/Controllers/Semaphore/SemaphoreVC.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class SemaphoreVC: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let urls = [
+        URL(string: "https://via.placeholder.com/150/92c952")!,
+        URL(string: "https://via.placeholder.com/150/771796")!,
+        URL(string: "https://via.placeholder.com/150/24f355")!,
+        URL(string: "https://via.placeholder.com/150/d32776")!,
+        URL(string: "https://via.placeholder.com/150/f66b97")!
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        loadImages()
     }
 
+    func loadImages() {
+        for url in self.urls {
+            DispatchQueue.global().async { self.downloadImage(from: url) }
+        }
+    }
+
+    func downloadImage(from url: URL) {
+        print("Starting download from \(url)")
+        guard let imageData = try? Data(contentsOf: url) else { return }
+        let image = UIImage(data: imageData)
+        DispatchQueue.main.async { self.imageView.image = image }
+        print("Finished download from \(url)")
+    }
 }
